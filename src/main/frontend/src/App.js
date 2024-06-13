42423e58fa8e64692afa56328b3f147b706b4563
hoo5886 <hoo5886@gmail.com>
2024-06-13 20:06:06 +0900

    <Design> 레이아웃배치 <changed> .gitingnore에 yaml파일 설정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Tags from "./components/Tags";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header className="header"/>
+      <div className="main">
+
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Tags className="sidebar" />
+      </div>
     </div>
   );
 }
--- src/main/frontend/src/components/Header.jsx
+++ src/main/frontend/src/components/Header.jsx
@@ -3,15 +3,13 @@ import "./Header.css";
 const Header = () => {
   return (
       <header className="Header">
-        <div className="header_left">명후세계</div>
+        <div className="header_left">개인 블로그</div>
         <div className="header_right">
-          <div className={`img_section img_section_${emotionId}`}>
+          <div className="img_section">
             <img src="../resources/mhface.jpg"/>
           </div>
           <div className="info_section">
-            <div className="created_date">
-              {new Date().toLocaleDateString()}
-            </div>
+            김명후
           </div>
         </div>
       </header>
