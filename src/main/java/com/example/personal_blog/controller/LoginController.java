42e2e93b6146f72846e398d0e6c2bde2c5a4fe32
hoo5886 <hoo5886@gmail.com>
2024-08-01 18:48:56 +0900

    <feature> {    Spring Security, logout 기능 구현    in-memory DB redis와 Blacklist 방법 활용 }

--- src/main/java/com/example/personal_blog/controller/LoginController.java
+++ src/main/java/com/example/personal_blog/controller/LoginController.java
@@ -4,6 +4,9 @@ import com.example.personal_blog.dto.request.SignInRequest;
 import com.example.personal_blog.dto.request.SignUpRequest;
 import com.example.personal_blog.dto.response.JwtAuthenticationResponse;
 import com.example.personal_blog.service.AuthenticationService;
+import com.example.personal_blog.service.JwtBlacklistService;
+import com.example.personal_blog.service.JwtService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,7 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class LoginController {
 
+    private final JwtBlacklistService jwtBlacklistService;
     private final AuthenticationService authenticationService;
+    private final JwtService jwtService;
 
     @PostMapping("/signup")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest signUpRequest) {
@@ -30,5 +35,17 @@ public class LoginController {
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SignInRequest signInRequest) {
         log.info("로그인 요청: {}", signInRequest.loginId());
         return ResponseEntity.ok(authenticationService.signIn(signInRequest));
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request) {
+        log.info("로그아웃 요청: {}", request.getHeader("Authorization"));
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String jwt = authHeader.substring(7);
+            long expiration = jwtService.extractExpiration(jwt).getTime() - System.currentTimeMillis();
+            jwtBlacklistService.blacklistToken(jwt, expiration);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
